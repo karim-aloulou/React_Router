f149dd57dbe88aff4508d3868d3e155b51f32871
karim-aloulou <karim.aloulou@esprit.tn>
2021-09-23 15:42:05 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import {useState} from "react"
+import { Data } from './components/Data';
+import MoviesList from './components/moviesList/MoviesList';
+import AddMovie from './components/add/AddMovie';
+import Header from './components/header/Header';
+import { Route, Switch } from 'react-router-dom';
+import MovieInfo from './components/moviesList/MovieInfo';
+
+function App() {
+  const [movies, setMovies] = useState(Data)
+  const [nameSearch, setNameSearch] = useState("")
+  const [rateSearch, setRateSearch] = useState(Number)
+  const handleSearch = (e) => {
+    setNameSearch(e.target.value)
+    setRateSearch(e.target.value)
+  }
+  const handleMovie = (newMovie) => {
+      setMovies([...movies, newMovie])
+  }
+  return (
+    <div className="App">
+      
+      <Switch>
+        <Route exact path="/" render={(props) =>
+        <div>
+          <Header handleSearch={handleSearch} />
+          <MoviesList movies={movies} nameSearch={nameSearch} rateSearch={rateSearch} />
+          <AddMovie handleMovie={handleMovie}/>
+          
+        </div>
+        }/>
+        <Route exact path="/movie/:id" render={(props) => <MovieInfo movies={movies} {...props} />} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
+
